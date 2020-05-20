dea67e8dd70d4c7fc2873d636168f2b2a619cd89
Veselina Zatsepina <v.zatsepina@alpinadigital.ru>
2020-05-20 12:57:20 +0300

    feat: added palette

---
+++ PaletteForArtist/app/src/main/java/com/github/veselinazatchepina/paletteforartist/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.github.veselinazatchepina.paletteforartist
+
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.palette.graphics.Palette
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.palette_color.view.*
+import kotlin.math.floor
+
+/**
+ * Цель задачи: знакомство с [Palette]
+ *
+ * Приложение позволяет получить дипазон цветов в соответствии с выбранной областью.
+ * Область определяется квадратом, полученным из начальных и конечных координат touch event.
+ *
+ * Достаточно нарисовать диагональ квадрата.
+ *
+ * В примере не были обработаны различные углы touch event и отрисовка выбранной области.
+ */
+class MainActivity : AppCompatActivity(), View.OnTouchListener {
+
+    private var paletteBuilder: Palette.Builder? = null
+    private var startXTouchCoordinate = 0
+    private var startYTouchCoordinate = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        image.setImageResource(R.drawable.monsted_flowers)
+        definePalette()
+        image.setOnTouchListener(this)
+    }
+
+    /**
+     * Устанавливаем Bitmap изображения с которого хотим получить цвета.
+     */
+    private fun definePalette() {
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.monsted_flowers)
+        paletteBuilder = Palette.Builder(bitmap)
+    }
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+
+        val matrix = Matrix()
+        image.imageMatrix.invert(matrix)
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN // нажатие
+            -> {
+                getCurrentCoordinates(event, matrix).apply {
+                    startXTouchCoordinate = first
+                    startYTouchCoordinate = second
+                }
+            }
+            MotionEvent.ACTION_MOVE -> {
+            }
+            MotionEvent.ACTION_UP // отпускание
+                , MotionEvent.ACTION_CANCEL -> {
+
+                getCurrentCoordinates(event, matrix).apply {
+                    paletteBuilder?.setRegion(
+                        startXTouchCoordinate,
+                        startYTouchCoordinate,
+                        first,
+                        second
+                    )
+                    generatePalette()
+                }
+            }
+        }
+
+        return true
+    }
+
+    /**
+     * Получаем начальные и конечные координаты touch event относительно текущего изображения.
+     */
+    private fun getCurrentCoordinates(event: MotionEvent, matrix: Matrix): Pair<Int, Int> {
+        val touchEventCoordinates = floatArrayOf(event.x, event.y)
+        matrix.mapPoints(touchEventCoordinates)
+        return Pair(
+            floor(touchEventCoordinates[0].toDouble()).toInt(),
+            floor(touchEventCoordinates[1].toDouble()).toInt()
+        )
+    }
+
+    /**
+     * Асинхронно генерируем цвета из выбранной части изображения.
+     */
+    private fun generatePalette() {
+        paletteBuilder?.generate { palette: Palette? ->
+
+            palette?.getDominantColor(0)?.let {
+                card_color_1.card_color.setBackgroundColor(it)
+            }
+
+            palette?.getMutedColor(0)?.let {
+                card_color_2.card_color.setBackgroundColor(it)
+            }
+
+            palette?.getLightVibrantColor(0)?.let {
+                card_color_3.card_color.setBackgroundColor(it)
+            }
+
+            palette?.getLightMutedColor(0)?.let {
+                card_color_4.card_color.setBackgroundColor(it)
+            }
+
+            palette?.getDarkVibrantColor(0)?.let {
+                card_color_5.card_color.setBackgroundColor(it)
+            }
+
+            palette?.getDarkMutedColor(0)?.let {
+                card_color_6.card_color.setBackgroundColor(it)
+            }
+        }
+    }
+}
